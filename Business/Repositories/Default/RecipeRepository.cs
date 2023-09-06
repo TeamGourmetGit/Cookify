d4cb0d50812a6757afb7a3c0a40450eabe4c9d80
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2023-09-06 18:13:49 +0200

    fixed cosmos conf and added business layer

---
+++ Business/Repositories/Default/RecipeRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Azure.Cosmos;
+using Shared.Models;
+
+namespace Business.Repositories.Default
+{
+    public class RecipeRepository : IRepository<Recipe>
+    {
+        private readonly Container _container;
+
+        public RecipeRepository(string cosmosConnectionString, string databaseName, string containerName)
+        {
+            var client = new CosmosClient(cosmosConnectionString);
+            var database = client.GetDatabase(databaseName);
+            _container = database.GetContainer(containerName);
+        }
+
+        public async Task<Recipe> AddAsync(Recipe entity)
+        {
+            var response = await _container.CreateItemAsync(entity);
+            return response.Resource;
+        }
+
+        public async Task<Recipe> DeleteAsync(object id)
+        {
+            var response = await _container.DeleteItemAsync<Recipe>(id.ToString(), new PartitionKey(id.ToString()));
+            return response.Resource;
+        }
+
+        public async Task<IEnumerable<Recipe>> GetAllAsync(string? sort, string? filter, int? page, int? pageSize)
+        {
+            var query = new QueryDefinition("SELECT * FROM c");
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                // Implement sorting logic here
+                // For example: query.OrderBy("c.PropertyName");
+            }
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                // Implement filtering logic here
+                // For example: query.Where("c.PropertyName = @filterParam", new SqlParameter("@filterParam", filter));
+            }
+
+            var resultSetIterator = _container.GetItemQueryIterator<Recipe>(query);
+
+            var results = new List<Recipe>();
+            while (resultSetIterator.HasMoreResults)
+            {
+                var response = await resultSetIterator.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
+
+            if (page.HasValue && pageSize.HasValue)
+            {
+                // Implement paging logic here
+                // For example: results = results.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            }
+
+            return results;
+        }
+
+        public async Task<Recipe> GetByIdAsync(object id)
+        {  
+            var response = await _container.ReadItemAsync<Recipe>(id.ToString(), new PartitionKey(id.ToString()));
+            return response.Resource;
+        }
+
+        public async Task<Recipe> UpdateAsync(Recipe entity)
+        {
+            var response = await _container.UpsertItemAsync(entity);
+            return response.Resource;
+        }
+    }
+}
--- cosmos.tf
+++ cosmos.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cosmosdb_account" "CDBA-Cookify" {
-  name                = "cookify-cosmos-db-1"
+  name                = "CDBA-Cookify"
   location            = local.RGlocation
   resource_group_name = local.RGname
   offer_type          = "Standard"
@@ -19,7 +19,7 @@ resource "azurerm_cosmosdb_account" "CDBA-Cookify" {
 
 
 resource "azurerm_cosmosdb_sql_database" "SQLDB-Cookify" {
-  name                = "SQLDB-Cookify"
+  name                = "cookify-cosmos-db-1"
   resource_group_name = local.RGname
   account_name        = azurerm_cosmosdb_account.CDBA-Cookify.name
   
@@ -32,30 +32,9 @@ resource "azurerm_cosmosdb_sql_container" "recipe-container" {
   resource_group_name   = local.RGname
   account_name          = azurerm_cosmosdb_account.CDBA-Cookify.name
   database_name         = azurerm_cosmosdb_sql_database.SQLDB-Cookify.name
-  partition_key_path    = "/definition/id"
+  partition_key_path    = "/partitionKey"
   partition_key_version = 1
   throughput            = 400
-  
-
-  indexing_policy {
-    indexing_mode = "consistent"
-
-    included_path {
-      path = "/*"
-    }
-
-    included_path {
-      path = "/included/?"
-    }
-
-    excluded_path {
-      path = "/excluded/?"
-    }
-  }
-
-  unique_key {
-    paths = ["/definition/idlong", "/definition/idshort"]
-  }
 
   depends_on = [ azurerm_cosmosdb_sql_database.SQLDB-Cookify ]
 }
