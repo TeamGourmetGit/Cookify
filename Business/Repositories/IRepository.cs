35214c61c49bd3a2914c8b531422a2a990d81801
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2023-09-07 09:50:13 +0200

    added new page to website

--- Business/Repositories/IRepository.cs
+++ Business/Repositories/IRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Business.Repositories.Default
+namespace Business.Repositories
 {
     public interface IRepository<T>
     {
