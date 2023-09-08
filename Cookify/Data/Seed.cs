8dd9c12da34d70a0388c9d8c218942e797597530
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2023-09-08 08:35:36 +0200

    added Seed and Recipe UI

---
+++ Cookify/Data/Seed.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+using Shared.Models;
+using System.Net;
+
+namespace Cookify.Data
+{
+    public class Seed
+    {
+        private readonly CosmosClient cosmosClient;
+        private readonly Database database;
+        private readonly Container container;
+
+        public Seed(string connectionString, string database, string container)
+        {
+            this.cosmosClient = new CosmosClient(connectionString);
+            this.database = this.cosmosClient.GetDatabase(database);
+            this.container = this.database.GetContainer(container);
+        }
+
+        public async Task InitializeAsync()
+        {
+            await AddRecipesToContainerAsync();
+        }
+
+
+        private async Task AddRecipesToContainerAsync()
+        {
+            var recipes = new List<Recipe>
+    {
+        new Recipe
+        {
+            Id = "1",
+            PartitionKey = "Italian",
+            Ingredients = new List<string>
+            {
+                "200g spaghetti",
+                "100g pancetta or guanciale",
+                "2 large eggs",
+                "50g Pecorino Romano cheese",
+                "Salt and black pepper"
+            },
+            Name = "Spaghetti Carbonara"
+        },
+        new Recipe
+        {
+            Id = "2",
+            PartitionKey = "Italian",
+            Ingredients = new List<string>
+            {
+                "8 oz fettuccine",
+                "2 boneless, skinless chicken breasts",
+                "2 cups heavy cream",
+                "1 cup grated Parmesan cheese",
+                "2 cloves garlic, minced",
+                "2 tablespoons butter",
+                "Salt and pepper to taste"
+            },
+            Name = "Chicken Alfredo"
+        },
+        new Recipe
+        {
+            Id = "3",
+            PartitionKey = "Asian",
+            Ingredients = new List<string>
+            {
+                "2 cups mixed vegetables (e.g., bell peppers, broccoli, carrots)",
+                "1 lb tofu or chicken, cubed",
+                "2 cloves garlic, minced",
+                "1/4 cup soy sauce",
+                "2 tablespoons vegetable oil",
+                "1 tablespoon honey or sugar"
+            },
+            Name = "Vegetable Stir-Fry"
+        },
+    };
+
+            foreach (var recipe in recipes)
+            {
+                try
+                {
+                    ItemResponse<Recipe> recipeResponse = await this.container.ReadItemAsync<Recipe>(recipe.Id, new PartitionKey(recipe.PartitionKey));
+                    Console.WriteLine("Item in database with id: {0} already exists\n", recipeResponse.Resource.Id);
+                }
+                catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+                {
+                    ItemResponse<Recipe> recipeResponse = await this.container.CreateItemAsync<Recipe>(recipe, new PartitionKey(recipe.PartitionKey));
+                    Console.WriteLine("Created item in database with id: {0}. Operation consumed {1} RUs.\n", recipeResponse.Resource.Id, recipeResponse.RequestCharge);
+                }
+            }
+        }
+
+    }
+}
---
+++ Cookify/Endpoints/RecipeEndpoints.cs
@@ -0,0 +1,28 @@
+﻿using Business.Repositories;
+using Cookify.Helpers;
+using Shared.Models;
+
+namespace Cookify.Endpoints
+{
+    public static class RecipeEndpoints
+    {
+        private static readonly IRepository<Recipe> _recipeRepository;
+
+        static RecipeEndpoints()
+        {
+            _recipeRepository = ServiceLocator.GetService<IRepository<Recipe>>();
+        }
+
+        public static WebApplication MapRecipeEndpoints(this WebApplication app)
+        {
+            app.MapGet("/api/recipes", GetAllRecipes).WithTags("Recipes");
+            return app;
+        }
+
+        public static async Task<IResult> GetAllRecipes(string? sort, string? filter, int? page = 1, int? pageSize = 100)
+        {
+            var models = await _recipeRepository.GetAllAsync(sort, filter, page, pageSize);
+            return Results.Ok(models);
+        }
+    }
+}
