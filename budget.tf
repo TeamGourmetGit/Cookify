a9a071c7cf4bc54d614547e882bc1694e4fcc6e7
Mooortie <Mattias.stromon@gmail.com>
2023-09-11 14:57:39 +0200

    fixed budget again

--- budget.tf
+++ budget.tf
@@ -13,7 +13,7 @@ resource "azurerm_consumption_budget_resource_group" "CookifytRGBudget" {
   time_grain = "Monthly"
 
   time_period {
-    start_date = "2023-09-11T00:00:00Z"
+    start_date = "2023-09-01T00:00:00Z"
     end_date   = "2024-09-11T00:00:00Z"
   }
 
